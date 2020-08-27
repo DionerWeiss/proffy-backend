870e58becb8903630fa7eae501e717890b9098a8
DionerWeiss <dionerweiss@gmail.com>
2020-08-27 20:17:08 -0300

    configuracao heroku

--- src/routes.ts
+++ src/routes.ts
@@ -7,6 +7,10 @@ const routes = express.Router();
 const classesController = new ClassesController;
 const connectionController = new ConnectionController;
 
+
+routes.get('/', (request, response) => {
+    return response.json({ "msg": "bem-vindo" })
+})
 routes.post('/classes', classesController.create);
 routes.get('/classes', classesController.index);
 routes.post('/connections', connectionController.create);
